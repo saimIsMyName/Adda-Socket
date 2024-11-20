a4c0fb1a09f8a7061cd464f09f8cc28e01177929
Saiful Islam Saim <saifulislamsaim1358@gmail.com>
2024-11-20 14:09:05 +0600

    Initialized

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const app = require("express")();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Server is ready for ADDA !");
+});
+
+const server = app.listen(
+  PORT,
+  console.log(`Server Running on PORT ${PORT}...`)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: [process.env.ORIGIN_DEV, process.env.ORIGIN_PRODUCTION],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    socket.leave(userData._id);
+  });
+});
